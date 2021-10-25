33e5254559d9c076e903f275e5451a81e25cfa2c
alexis WALTER <alexis.walter@epitech.eu>
2021-10-25 17:01:24 +0200

    test requirement.txt

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def runPrediction(model, image):
     # # prepare pixel data
     # img = img.astype('float32')
     # img = img / 255.0
-    return img
+    return img.shape
 
 
 def file_selector(folder_path='.'):
