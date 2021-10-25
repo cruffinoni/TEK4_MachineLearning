4750bd6ab14aa397ead3d0d9019d82e8ffafa785
cruffinoni <clement.ruffinoni@epitech.eu>
2021-10-25 11:51:04 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,71 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import pickle
+import warnings
+
+st.beta_set_page_config(page_title="Handwritten number recognition", page_icon="✍",
+                        layout='centered', initial_sidebar_state="collapsed")
+
+
+def load_model(modelfile):
+    loaded_model = pickle.load(open(modelfile, 'rb'))
+    return loaded_model
+
+
+def file_selector(folder_path='.'):
+    filenames = os.listdir(folder_path)
+    selected_filename = st.selectbox('Select a file', filenames)
+    return os.path.join(folder_path, selected_filename)
+
+def main():
+    # title
+    st.markdown("""
+    <div>
+    <h1 style="color:MEDIUMSEAGREEN;text-align:left;"> Handwritten number 🔢 </h1>
+    </div>
+    """, unsafe_allow_html=True)
+
+    col1, col2 = st.beta_columns([2, 2])
+
+    with col1:
+        with st.beta_expander(" ℹ️ Information", expanded=True):
+            st.write("""
+           Definition of the project
+
+            """)
+        '''
+        ## How does it work ❓ 
+        You can upload an 80x80 image with a black background and white writing. Then press the prediction button and the result will be displayed at the bottom.
+        '''
+
+    with col2:
+        uploaded_file = st.file_uploader("Upload Files", type=['png', 'jpeg'])
+        if uploaded_file is not None:
+            file_details = {"FileName": uploaded_file.name,
+                            "FileType": uploaded_file.type,
+                            "FileSize": uploaded_file.size}
+            st.write(file_details)
+
+        if st.button('Predict'):
+            col1.write('''
+            ## Results 🔍 
+            ''')
+            col1.success(
+                f"{5} are recommended by the A.I for your farm.")
+    # code for html ☘️ 🌾 🌳 👨‍🌾  🍃
+
+    st.warning(
+        "Note: This A.I application is for educational/demo purposes only and cannot be relied upon.")
+
+    hide_menu_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+
+if __name__ == '__main__':
+    main()
