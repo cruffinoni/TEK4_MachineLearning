bcea702bbfefb4952e82b2c47627c2f14146d91e
cruffinoni <clement.ruffinoni@epitech.eu>
2021-10-25 18:59:17 +0200

    test center contentv2

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ def main():
 
     with col2:
         with st.beta_expander("🤖  Artificial intelligence", expanded=True):
-            st.markdown("<div style=\"align:center\">", True)
-            st.write("""
+            st.write("<div style=\"align:center\">", True)
+            st.markdown("""
             #### How does it work ❓ 
                 You can draw a number between 0 and 9 and our model is going to try to recognize the number that you draw.
             """)
@@ -76,7 +76,7 @@ def main():
             drawing_mode="freedraw",
             key="canvas",
         )
-        st.markdown("</div>", True)
+        st.write("</div>", True)
         if st.button('Predict') and canvas_result.image_data is not None:
             col1.write('''
             ## Results 🔍 
