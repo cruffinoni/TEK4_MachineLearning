e7b566ab689d53e244da09e90ba24738a376d0e3
alexis WALTER <alexis.walter@epitech.eu>
2021-10-25 16:54:50 +0200

    test requirement.txt

--- main.py
+++ main.py
@@ -21,12 +21,11 @@ st.beta_set_page_config(page_title="Handwritten number recognition", page_icon="
 
 def runPrediction(model, image):
     im = Image.fromarray((image * 255).astype(np.uint8))
-    im.save("currentImage", "PNG")
 
     # load the image
-    img = load_img("currentImage.png", grayscale=True, target_size=(28, 28))
+    # img = load_img("currentImage.png", grayscale=True, target_size=(28, 28))
     # convert to array
-    img = img_to_array(img)
+    img = img_to_array(im)
     # reshape into a single sample with 1 channel
     img = img.reshape(1, 28, 28, 1)
     # prepare pixel data
